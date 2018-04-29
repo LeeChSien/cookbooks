6af6beda61aad69a948462dc268aadca5dbf9c29
Jason Lee <chsienlee@gmail.com>
2018-04-30 00:00:46 +0800

    Fix

---
+++ chinese_fonts/recipes/default.rb
@@ -0,0 +1,1 @@
+include_recipe 'chinese_fonts::setup'
---
+++ chinese_fonts/recipes/setup.rb
@@ -0,0 +1,20 @@
+
+bash "grab chinese fonts" do
+
+  user 'root'
+  group 'root'
+  code <<-EOH
+    URL=https://s3.ap-northeast-1.amazonaws.com/chinese-fonts/fonts.zip
+
+    CACHE_DIR=/usr/tmp
+    FILE_NAME=fonts.zip
+    DEST=${CACHE_DIR}/${FILE_NAME}
+
+    curl $URL -o $CACHE_DIR
+
+    unzip ${CACHE_DIR}/${FILE_NAME} -C ${CACHE_DIR}/fonts
+    mv ${CACHE_DIR}/fonts/*.ttc  /usr/share/fonts/
+
+    EOH
+
+end
