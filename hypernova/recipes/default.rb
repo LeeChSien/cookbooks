907e1eaa56963b5df6b8a30c76ee6ae56bde04df
Jason Lee <chsienlee@gmail.com>
2018-07-04 20:43:55 +0800

    Fix

--- hypernova/recipes/default.rb
+++ hypernova/recipes/default.rb
@@ -7,8 +7,8 @@ bash "hypernova" do
         source ~/.bashrc
         cd /srv/www/meetjobs_frontend/current/
 
-        node app/config/ssr/hypernova.jsx > /dev/null &
-        http-server build -p 80 -a 0.0.0.0 > /dev/null &
+        nohup node app/config/ssr/hypernova.jsx > /dev/null &
+        nohup http-server build -p 80 -a 0.0.0.0 > /dev/null &
 
     EOH
   
