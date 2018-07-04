2a75e706568d803d7942a6c176833382e44f2e89
Jason Lee <chsienlee@gmail.com>
2018-07-04 19:08:01 +0800

    Add hypernova

--- hypernova/recipes/restart.rb
+++ hypernova/recipes/restart.rb
@@ -9,8 +9,8 @@ bash "restart" do
 
         cd /srv/www/meetjobs_frontend/current/
 
-        nohup node app/config/ssr/hypernova.jsx &
-        nohup http-server build -p 80 -a 0.0.0.0 &
+        nohup node app/config/ssr/hypernova.jsx > /dev/null &
+        nohup http-server build -p 80 -a 0.0.0.0 > /dev/null &
 
     EOH
   
