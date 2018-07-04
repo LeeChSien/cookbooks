161fb6edc7895b15989c883f4e24e1684eb40dbe
Jason Lee <chsienlee@gmail.com>
2018-07-04 19:19:45 +0800

    Fix

--- hypernova/recipes/restart.rb
+++ hypernova/recipes/restart.rb
@@ -4,13 +4,10 @@ bash "restart" do
     group 'root'
     code <<-EOH
       
-        kill -9 $(ps aux | grep '[n]ode app/config/ssr/hypernova.jsx' | awk '{print $2}')
-        kill -9 $(ps aux | grep '[n]ode /root/.nvm/versions/node/v9.11.1/bin/http-server build -p 80 -a 0.0.0.0' | awk '{print $2}')
-
         cd /srv/www/meetjobs_frontend/current/
 
-        nohup node app/config/ssr/hypernova.jsx > /dev/null &
-        nohup http-server build -p 80 -a 0.0.0.0 > /dev/null &
+        node app/config/ssr/hypernova.jsx > /dev/null &
+        http-server build -p 80 -a 0.0.0.0 > /dev/null &
 
     EOH
   
